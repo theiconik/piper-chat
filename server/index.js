6ddf9926eb6cd6f513fb7e153f8bc9726d7db9d4
theiconik <kumarujjawal51@gmail.com>
2021-05-30 19:57:23 +0530

    Server end coded (Socket.io)

---
+++ server/index.js
@@ -0,0 +1,38 @@
+//Server Side Code
+
+const app = require("express")();
+const server = require("http").createServer(app);
+const cors = require("cors");
+
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Server is running!");
+});
+
+io.on("connection", (socket) => {
+  socket.emit("me", socket.id);
+
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("call-ended");
+  });
+
+  socket.on("call-user", ({ userToCall, signalData, from, name }) => {
+    io.to(userToCall).emit("call-user", { signal: signalData, from, name });
+  });
+
+  socket.on("answer-call", (data) => {
+    io.to(data.to).emit("call-accepted", data.signal);
+  });
+});
+
+server.listen(PORT, () => console.log("Server listening on port " + PORT));
