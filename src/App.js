a6ae36dcf2ca66da86d4b46caa503f5b81fbef45
theiconik <kumarujjawal51@gmail.com>
2021-05-30 23:50:22 +0530

    footer, fonts, icons and packges added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Hello world!
+      <footer>
+      🤍<span style={{color: '#89a2a1'}}> pied</span><span style={{color: '#6fdfb8'}}>piper</span>
+      </footer>
     </div>
   );
 }
