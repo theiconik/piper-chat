1a1f532186aeeda4159fc05bcc981f304ce33d98
theiconik <kumarujjawal51@gmail.com>
2021-05-31 00:10:50 +0530

    heading and material ui added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import { Typography, AppBar } from '@material-ui/core';
 
 function App() {
   return (
     <div className="App">
+      
+      <h1 className="heading"><i class="fab fa-pied-piper"></i> piper chat</h1>
+
+      {/* Video Player */}
+      
       <footer>
       🤍<span style={{color: '#89a2a1'}}> pied</span><span style={{color: '#6fdfb8'}}>piper</span>
       </footer>
