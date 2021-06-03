fc7d216c9265c5c33d8412ea467ee0ba204cd709
theiconik <kumarujjawal51@gmail.com>
2021-06-04 02:58:17 +0530

    Whole server changed

--- src/SocketContext.js
+++ src/SocketContext.js
@@ -5,7 +5,7 @@ import Peer from 'simple-peer';
 const SocketContext = createContext();
 
 // const socket = io('http://localhost:5000');
- const socket = io('https://video-chat-app-jsm.herokuapp.com');
+ const socket = io('https://pied-piper-chat-server.herokuapp.com/');
 
 const ContextProvider = ({ children }) => {
   const [callAccepted, setCallAccepted] = useState(false);
